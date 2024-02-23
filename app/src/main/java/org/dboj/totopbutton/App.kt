d1b003fb97f759267677990124f8587ae1a22195
Daniel Boj <dboj@uoc.edu>
2024-02-23 12:01:40 +0100

    Project Upload: v1.1.1

--- app/src/main/java/org/dboj/totopbutton/App.kt
+++ app/src/main/java/org/dboj/totopbutton/App.kt
@@ -1,18 +1,5 @@
 package org.dboj.totopbutton
 
 import android.app.Application
-import coil.ImageLoader
-import coil.ImageLoaderFactory
 
-class App: Application(), ImageLoaderFactory {
-
-    override fun newImageLoader(): ImageLoader {
-        return ImageLoader(this)
-            .newBuilder()
-            .components {
-
-            }
-            .build()
-
-    }
-}
+class App: Application()
